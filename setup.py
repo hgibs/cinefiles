c1d9591e692281f167c65806697b9ce3982fae9f
ArcAwe-MBP <holland.gibson@gmail.com>
2017-03-31 23:06:45 +0430

    disabled scripts for now

--- setup.py
+++ setup.py
@@ -19,10 +19,7 @@ with open(path.join(here, 'PYPI-README.rst'), encoding='utf-8') as f:
 
 setup(
     name='cinefiles',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
+    
     version='0.1.4',
 
     description='Organize your movie folder and files',
@@ -44,7 +41,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: End Users/Desktop',
@@ -61,32 +58,19 @@ setup(
  #        'Programming Language :: Python :: 2',
 #         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+#         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 
-    # What does your project relate to?
     keywords='movies organization folder',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-#     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    entry_points = {'console_scripts': ['cinefiles=cinefiles.__main__:main_cfiles',
-                                        'cinefolders=cinefiles.__main__:main_cfolders'],},
-#     scripts=['bin/cinefiles'],
+#     entry_points = {'console_scripts': ['cinefiles=cinefiles.__main__:main_cfiles',
+#                                         'cinefolders=cinefiles.__main__:main_cfolders'],},
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[  'requests<3,>=2.4.3',
                         'youtube_dl>=2017.2.17',
                         'guessit<3,>=2.1.1',
@@ -94,37 +78,11 @@ setup(
                         'google-api-python-client<2,>=1.6.2',
                         'lxml<4,>=3.7.3',
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
+    
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['twine'],
         'test': ['codecov','pytest','pytest-pep8','pytest-cov','pytest-console-scripts'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-#     package_data={
-#         'sample': ['package_data.dat'],
-#     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-#     data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-#     entry_points={
-#         'console_scripts': [
-#             'sample=sample:main',
-#         ],
-#     },
 )
-
 #codecov skip end
